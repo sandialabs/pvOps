b4d5a8417857a350a0420296a1851b0c62bd0e17
Hopwood <mwhopwo@sandia.gov>
2021-05-10 16:44:16 -0400

    lint utils

--- pvops/iv/utils.py
+++ pvops/iv/utils.py
@@ -17,24 +17,29 @@ def get_CEC_params(name, mod_spec):
     '''
 
     moddb = pvsystem.retrieve_sam('CECMod')
-    module_parameters =moddb[name].to_dict()
+    module_parameters = moddb[name].to_dict()
 
     # add reverse bias parameters
     module_parameters['breakdown_factor'] = 1.e-4
-    module_parameters['breakdown_voltage'] = -30.#-5.5
+    module_parameters['breakdown_voltage'] = -30.  # -5.5
     module_parameters['breakdown_exp'] = 3.28
     module_parameters['ncols'] = mod_spec['ncols']
     module_parameters['nsubstrings'] = mod_spec['nsubstrings']
-    module_parameters['ncells_substring'] = module_parameters['N_s'] / mod_spec['nsubstrings']
-    module_parameters['nrows'] = module_parameters['N_s'] / module_parameters['ncols']
-    #module_parameters['R_sh_ref'] *= rsh_premultiply # What should this value be? Dynamic.
-    #TODO: Adjust Io smaller
+    module_parameters['ncells_substring'] = module_parameters['N_s'] / \
+        mod_spec['nsubstrings']
+    module_parameters['nrows'] = module_parameters['N_s'] / \
+        module_parameters['ncols']
+    # module_parameters['R_sh_ref'] *= rsh_premultiply # What should this value be? Dynamic.
+    # TODO: Adjust Io smaller
 
     #module_parameters['R_s'] /= 1.5
 
     # set up cell-level parameters
     cell_parameters = copy.copy(module_parameters)
-    cell_parameters['a_ref'] = module_parameters['a_ref'] / module_parameters['N_s']
-    cell_parameters['R_sh_ref'] = module_parameters['R_sh_ref'] / module_parameters['N_s']
-    cell_parameters['R_s'] = module_parameters['R_s'] / module_parameters['N_s']
+    cell_parameters['a_ref'] = module_parameters['a_ref'] / \
+        module_parameters['N_s']
+    cell_parameters['R_sh_ref'] = module_parameters['R_sh_ref'] / \
+        module_parameters['N_s']
+    cell_parameters['R_s'] = module_parameters['R_s'] / \
+        module_parameters['N_s']
     return module_parameters, cell_parameters
