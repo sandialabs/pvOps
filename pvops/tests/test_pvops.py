f8516204f0b210ef0e1c236f49bdb365ee64d698
Hopwood <mwhopwo@sandia.gov>
2021-05-10 13:53:39 -0400

    manual linting x2

--- pvops/tests/test_pvops.py
+++
@@ -1,2 +0,0 @@
-def test_it_works():
-    assert True
--- pvops/tests/test_text2time.py
+++ pvops/tests/test_text2time.py
@@ -63,7 +63,7 @@ prod_data = pd.read_csv(example_prodpath, error_bad_lines=False, engine='python'
 om_data = pd.read_csv(example_OMpath, error_bad_lines=False, engine='python')
 metadata = pd.read_csv(example_metapath, error_bad_lines=False, engine='python')
 
-def check_same(df1,df2,col):
+def check_same(df1, df2, col):
     if (df1[col].isnull().all() and df2[col].isnull().all()):
         # If both are all nan in col, then assert True
         assert True
@@ -77,12 +77,11 @@ def check_same(df1,df2,col):
 def test_om_data_convert_s():
     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
     assert ptypes.is_datetime64_dtype(om_data_converted[om_col_dict['datestart']])
-    
+
 def test_om_data_convert_e():
-    
     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
     assert ptypes.is_datetime64_dtype(om_data_converted[om_col_dict['dateend']])
-    
+
 def test_prod_data_convert():
     prod_data_converted = preprocess.prod_date_convert(prod_data, prod_col_dict)
     assert ptypes.is_datetime64_dtype(prod_data_converted[prod_col_dict['timestamp']])
@@ -91,7 +90,7 @@ def test_prod_data_convert():
 #     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
 #     om_data_sitena, addressed = preprocess.data_site_na(om_data_converted, om_col_dict)
 #     assert sum(om_data_sitena.loc[prod_col_dict['siteid']].isna())==0
-    
+
 def test_om_datelogic_s():
     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
     om_data_sitena, addressed = preprocess.data_site_na(om_data_converted, om_col_dict)
@@ -114,7 +113,7 @@ def test_prod_anomalies_ffT():
                                                      np.nan, ffill=True)
     mask = prod_data_anom[prod_col_dict['energyprod']] < 1.0
     assert sum(mask) == 0
-    
+
 def test_prod_anomalies_ffF():
     threshold= 1.0
     prod_data_converted = preprocess.prod_date_convert(prod_data, prod_col_dict)
@@ -123,7 +122,7 @@ def test_prod_anomalies_ffF():
                                                      np.nan, ffill=False)
     mask = prod_data_anom[prod_col_dict['energyprod']] < 1.0
     assert sum(mask) == 0
-    
+
 def test_prod_nadate_process_d():
     threshold = 1.0
     prod_data_converted = preprocess.prod_date_convert(prod_data, prod_col_dict)
@@ -135,7 +134,7 @@ def test_prod_nadate_process_d():
                                                                  pnadrop=True)
     mask = prod_data_datena[prod_col_dict['timestamp']].isna()
     assert sum(mask) == 0
-    
+
 def test_prod_nadate_process_id():
     threshold = 1.0
     prod_data_converted = preprocess.prod_date_convert(prod_data, prod_col_dict)
@@ -180,7 +179,7 @@ def test_summarize_overlaps():
     prod_data_datena_d, addressed = preprocess.prod_nadate_process(prod_data_anom,
                                                                    prod_col_dict,
                                                                    pnadrop=True)
-    
+
     # O&M data => Note the flags used
     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
     om_data_sitena, addressed = preprocess.data_site_na(om_data_converted, om_col_dict)
@@ -188,14 +187,14 @@ def test_summarize_overlaps():
                                                                  om_col_dict,'swap')
     om_data_datena_d, addressed = preprocess.om_nadate_process(om_data_checked_s, om_col_dict,
                                                                om_dendflag='drop')
-    
+
     # summarize overlaps
     prod_summary, om_summary = utils.summarize_overlaps(prod_data_datena_d, om_data_datena_d, prod_col_dict, om_col_dict)
-    
+
     # import expected pickled DFs
     prod_summ_pick = pd.read_pickle(os.path.join(test_datadir, 'prod_summ_pick.pkl'))
     om_summ_pick = pd.read_pickle(os.path.join(test_datadir, 'om_summ_pick.pkl'))
-    
+
     assert prod_summary.equals(prod_summ_pick) and om_summary.equals(om_summ_pick)
     
     
@@ -205,16 +204,16 @@ def test_overlapping_data():
     prod_data_converted = preprocess.prod_date_convert(prod_data, prod_col_dict)
     prod_data_anom, addressed = utils.prod_anomalies(prod_data_converted, prod_col_dict, threshold, np.nan, ffill=True)
     prod_data_datena_d, addressed = preprocess.prod_nadate_process(prod_data_anom, prod_col_dict, pnadrop=True)
-    
+
     # O&M data
     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
     om_data_sitena, addressed = preprocess.data_site_na(om_data_converted, om_col_dict)
     om_data_checked_s, addressed = preprocess.om_datelogic_check(om_data_sitena, om_col_dict, 'swap')
     om_data_datena_d, addressed = preprocess.om_nadate_process(om_data_checked_s, om_col_dict, om_dendflag='drop')
-    
+
     # trim DFs
     prod_data_clean, om_data_clean = utils.overlapping_data(prod_data_datena_d, om_data_datena_d, prod_col_dict, om_col_dict)
-    
+
     assert len(prod_data_clean) == 1020 and len(om_data_clean) == 7
 
 
@@ -224,86 +223,116 @@ def test_iec_calc():
     prod_data_converted = preprocess.prod_date_convert(prod_data, prod_col_dict)
     prod_data_anom, addressed = utils.prod_anomalies(prod_data_converted, prod_col_dict, threshold, np.nan, ffill=True)
     prod_data_datena_d, addressed = preprocess.prod_nadate_process(prod_data_anom, prod_col_dict, pnadrop=True)
-    
+
     # O&M data
     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
     om_data_sitena, addressed = preprocess.data_site_na(om_data_converted, om_col_dict)
     om_data_checked_s, addressed = preprocess.om_datelogic_check(om_data_sitena, om_col_dict, 'swap')
     om_data_datena_d, addressed = preprocess.om_nadate_process(om_data_checked_s, om_col_dict, om_dendflag='drop')
-    
+
     # trim DFs
     prod_data_clean, om_data_clean = utils.overlapping_data(prod_data_datena_d, om_data_datena_d, prod_col_dict, om_col_dict)
-    
+
     # IEC calc
     prod_data_clean_iec = utils.iec_calc(prod_data_clean, prod_col_dict, metadata, metad_col_dict, gi_ref=1000.)
-    
+
     # import expected pickled DFs
     prod_data_clean_iec_pick = pd.read_pickle(os.path.join(test_datadir, 'prod_data_clean_iec_pick.pkl'))
-    
+
     for col in prod_data_clean_iec_pick.columns:
-        check_same(prod_data_clean_iec_pick,prod_data_clean_iec_pick,col)
+        check_same(prod_data_clean_iec_pick, prod_data_clean_iec_pick, col)
 
 
 # def test_prod_quant():
-    
+
 #     #Prod data
 #     threshold = 1.0
-#     prod_data_converted = preprocess.prod_date_convert(prod_data, prod_col_dict)
-#     prod_data_anom, addressed = utils.prod_anomalies(prod_data_converted, prod_col_dict, threshold, np.nan, ffill=True)
-#     prod_data_datena_d, addressed = preprocess.prod_nadate_process(prod_data_anom, prod_col_dict, pnadrop=True)
-    
+#     prod_data_converted = preprocess.prod_date_convert(prod_data,
+#                                                        prod_col_dict)
+#     prod_data_anom, addressed = utils.prod_anomalies(prod_data_converted,
+#                                                      prod_col_dict, threshold,
+#                                                      np.nan, ffill=True)
+#     prod_data_datena_d, addressed = preprocess.prod_nadate_process(prod_data_anom,
+#                                                                    prod_col_dict,
+#                                                                    pnadrop=True)
+
 #     #O&M data
 #     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
-#     om_data_sitena, addressed = preprocess.data_site_na(om_data_converted, om_col_dict)
-#     om_data_checked_s, addressed = preprocess.om_datelogic_check(om_data_sitena, om_col_dict, 'swap')
-#     om_data_datena_d, addressed = preprocess.om_nadate_process(om_data_checked_s, om_col_dict, om_dendflag='drop')
-    
+#     om_data_sitena, addressed = preprocess.data_site_na(om_data_converted,
+#                                                         om_col_dict)
+#     om_data_checked_s, addressed = preprocess.om_datelogic_check(om_data_sitena,
+#                                                                  om_col_dict, 'swap')
+#     om_data_datena_d, addressed = preprocess.om_nadate_process(om_data_checked_s,
+#                                                                om_col_dict,
+#                                                                om_dendflag='drop')
+
 #     #trim DFs
-#     prod_data_clean, om_data_clean = utils.overlapping_data(prod_data_datena_d, om_data_datena_d, prod_col_dict, om_col_dict)
-    
+#     prod_data_clean, om_data_clean = utils.overlapping_data(prod_data_datena_d,
+#                                                             om_data_datena_d,
+#                                                             prod_col_dict,
+#                                                             om_col_dict)
+
 #     #IEC calc
-#     prod_data_clean_iec = utils.iec_calc(prod_data_clean, prod_col_dict, metadata, metad_col_dict, gi_ref=1000.)
-    
-#     prod_data_quant = utils.prod_quant(prod_data_clean_iec, prod_col_dict, comp_type='norm', ecumu=True)
-    
+#     prod_data_clean_iec = utils.iec_calc(prod_data_clean, prod_col_dict,
+#                                          metadata, metad_col_dict,
+#                                          gi_ref=1000.)
+
+#     prod_data_quant = utils.prod_quant(prod_data_clean_iec, prod_col_dict,
+#                                        comp_type='norm', ecumu=True)
+
 #     #import expected pickled DFs
-#     prod_data_quant_pick = pd.read_pickle(os.path.join(test_datadir, 'prod_data_quant_pick.pkl'))
-    
+#     prod_data_quant_pick = pd.read_pickle(os.path.join(test_datadir,
+#                                                        'prod_data_quant_pick.pkl'))
+
 #     print(prod_data_quant)#.dtypes)
 #     print(prod_data_quant_pick)#.dtypes)
-    
+
 #     for col in prod_data_quant_pick.columns:
 #         check_same(prod_data_quant,prod_data_quant_pick,col)
     
     
 def test_om_summary_stats():
-    
+
     # Prod data
     threshold = 1.0
     prod_data_converted = preprocess.prod_date_convert(prod_data, prod_col_dict)
-    prod_data_anom, addressed = utils.prod_anomalies(prod_data_converted, prod_col_dict, threshold, np.nan, ffill=True)
-    prod_data_datena_d, addressed = preprocess.prod_nadate_process(prod_data_anom, prod_col_dict, pnadrop=True)
-    
+    prod_data_anom, addressed = utils.prod_anomalies(prod_data_converted,
+                                                     prod_col_dict, threshold,
+                                                     np.nan, ffill=True)
+    prod_data_datena_d, addressed = preprocess.prod_nadate_process(prod_data_anom,
+                                                                   prod_col_dict,
+                                                                   pnadrop=True)
+
     # O&M data
     om_data_converted = preprocess.om_date_convert(om_data, om_col_dict)
-    om_data_sitena, addressed = preprocess.data_site_na(om_data_converted, om_col_dict)
-    om_data_checked_s, addressed = preprocess.om_datelogic_check(om_data_sitena, om_col_dict, 'swap')
-    om_data_datena_d, addressed = preprocess.om_nadate_process(om_data_checked_s, om_col_dict, om_dendflag='drop')
-    
+    om_data_sitena, addressed = preprocess.data_site_na(om_data_converted, 
+                                                        om_col_dict)
+    om_data_checked_s, addressed = preprocess.om_datelogic_check(om_data_sitena,
+                                                                 om_col_dict,
+                                                                 'swap')
+    om_data_datena_d, addressed = preprocess.om_nadate_process(om_data_checked_s,
+                                                               om_col_dict,
+                                                               om_dendflag='drop')
+
     # trim DFs
-    prod_data_clean, om_data_clean = utils.overlapping_data(prod_data_datena_d, om_data_datena_d, prod_col_dict, om_col_dict)
-    
+    prod_data_clean, om_data_clean = utils.overlapping_data(prod_data_datena_d,
+                                                            om_data_datena_d,
+                                                            prod_col_dict,
+                                                            om_col_dict)
+
     # OM Stats Calc
-    om_data_update = utils.om_summary_stats(om_data_clean, metadata, om_col_dict, metad_col_dict)
-    
+    om_data_update = utils.om_summary_stats(om_data_clean, metadata,
+                                            om_col_dict, metad_col_dict)
+
     # import expected pickled DF
-    om_data_update_pick = pd.read_pickle(os.path.join(test_datadir, 'om_data_update_pick.pkl'))
-    om_data_update_pick = om_data_update_pick.round({'EventDur':2})
-    om_data_update = om_data_update.round({'EventDur':2})
-    
+    om_data_update_pick = pd.read_pickle(os.path.join(test_datadir,
+                                                      'om_data_update_pick.pkl'))
+    om_data_update_pick = om_data_update_pick.round({'EventDur': 2})
+    om_data_update = om_data_update.round({'EventDur': 2})
+
     om_data_update = om_data_update.astype({'MonthStart': 'int64'})
-    
+
     for col in om_data_update_pick.columns:
-        check_same(om_data_update,om_data_update_pick,col)
+        check_same(om_data_update, om_data_update_pick, col)
 
 # test_prod_quant()
