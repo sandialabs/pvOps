315beeb8daba87f9451df19ca59aa903f1e33ab6
MichaelHopwood <michael.hopwood@knights.ucf.edu>
2021-05-13 17:56:49 -0400

    configure tests for new timeseries dev

--- pvops/tests/test_timeseries.py
+++ pvops/tests/test_timeseries.py
@@ -2,9 +2,9 @@ import os
 import sys
 import pandas as pd
 
-timeseries_directory = os.path.join("pvops")
-sys.path.append(timeseries_directory)
-
+pvops_directory = os.path.join("pvops")
+sys.path.append(pvops_directory)
+from timeseries.models import linear
 from timeseries import preprocess as tprep
 from text2time import preprocess as t2tprep
 
@@ -13,6 +13,7 @@ datadir = os.path.join('examples', 'example_data')
 example_OMpath = os.path.join(datadir, 'example_om_data2.csv')
 example_prodpath = os.path.join(datadir, 'example_perf_data.csv')
 example_metapath = os.path.join(datadir, 'example_metadata2.csv')
+example_prod2path = os.path.join(datadir, 'example_prod_with_covariates.csv')
 
 # Assigning dictionaries to connect pvops variables with User's column names
 # Format for dictionaries is {pvops variable: user-specific column names}
@@ -21,13 +22,11 @@ prod_col_dict = {'siteid': 'randid',
                  'power': 'AC_POWER',
                  'energyprod': 'Energy',
                  'irradiance': 'POAirradiance',
-                 # user's name choice for new column (baseline expected energy defined by user or calculated based on IEC)
                  'baseline': 'IEC_pstep',
-                 # user's name choice for new column (System DC-size, extracted from meta-data)
                  'dcsize': 'dcsize',
-                 'compared': 'Compared',  # user's name choice for new column
-                 'energy_pstep': 'Energy_pstep',  # user's name choice for new column
-                 'clearsky_irr': 'clearsky_irr'  # user's name choice for new column
+                 'compared': 'Compared',
+                 'energy_pstep': 'Energy_pstep',
+                 'clearsky_irr': 'clearsky_irr'
                  }
 
 om_col_dict = {'siteid': 'randid',
@@ -36,11 +35,9 @@ om_col_dict = {'siteid': 'randid',
                'workID': 'WONumber',
                'worktype': 'WOType',
                'asset': 'Asset',
-               # user's name choice for new column (Repair Duration)
                'eventdur': 'EventDur',
-               # user's name choice for new column (Month when an event begins)
                'modatestart': 'MonthStart',
-               'agedatestart': 'AgeStart'}  # user's name choice for new column (Age of system when event begins)
+               'agedatestart': 'AgeStart'}
 
 metad_col_dict = {'siteid': 'randid',
                   'dcsize': 'DC_Size_kW',
@@ -62,8 +59,10 @@ def test_prod_irradiance_filter():
     # The first set of rows
     prod_df = prod_df.iloc[0:200]
 
-    prod_df_out, mask_series = tprep.prod_irradiance_filter(prod_df, prod_col_dict,
-                                                            meta_df, metad_col_dict)
+    prod_df_out, mask_series = tprep.prod_irradiance_filter(prod_df,
+                                                            prod_col_dict,
+                                                            meta_df,
+                                                            metad_col_dict)
 
     true_detection_irradiance = [0, 44]
     assert sum(mask_series) in true_detection_irradiance
@@ -104,3 +103,61 @@ def test_prod_inverter_clipping_filter():
     assert sum(geometric['mask']) == true_detection_geometric
     assert sum(threshold['mask']) == true_detection_threshold
     assert sum(levels['mask']) == true_detection_levels
+
+
+def test_linear_model():
+    prod_df = pd.read_csv(example_prod2path)
+    meta_df = pd.read_csv(example_metapath)
+
+    # Format for dictionaries is {pvops variable: user-specific column names}
+    prod_col_dict = {'siteid': 'randid',
+                     'timestamp': 'date',
+                     'powerprod': 'generated_kW',
+                     'irradiance': 'irrad_poa_Wm2',
+                     'temperature': 'temp_amb_C',
+                     'baseline': 'IEC_pstep',
+                     'dcsize': 'dcsize',
+                     'compared': 'Compared',
+                     'energy_pstep': 'Energy_pstep'}
+
+    metad_col_dict = {'siteid': 'randid',
+                      'dcsize': 'DC_Size_kW',
+                      'COD': 'COD'}
+
+    prod_data_converted = t2tprep.prod_date_convert(prod_df, prod_col_dict)
+    prod_data_datena_d, _ = t2tprep.prod_nadate_process(
+        prod_data_converted, prod_col_dict, pnadrop=True)
+
+    prod_data_datena_d.index = prod_data_datena_d[prod_col_dict['timestamp']]
+
+    model_prod_data = prod_data_datena_d.dropna(subset=[
+        'irrad_poa_Wm2', 'temp_amb_C', 'wind_speed_ms'] +
+        [prod_col_dict['powerprod']
+         ])
+    model_prod_data = model_prod_data[model_prod_data['randid'] == 'R15']
+
+    model, train_df, test_df = linear.modeller(model_prod_data,
+                                               prod_col_dict,
+                                               meta_df,
+                                               metad_col_dict,
+                                               kernel_type='default',
+                                               time_weighted='month',
+                                               X_parameters=[
+                                                   'irrad_poa_Wm2',
+                                                   'temp_amb_C'],
+                                               test_split=0.05,
+                                               degree=3,
+                                               verbose=0)
+
+    name = list(model.estimators.keys())[0]
+
+    benchmark_r2 = 0.99
+    benchmark_mse = 420000
+
+    eval = model.estimators[name]['test_eval']
+
+    assert eval['r2'] > benchmark_r2
+    assert eval['mse'] < benchmark_mse
+
+
+test_linear_model()
