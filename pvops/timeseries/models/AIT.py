4fd4e6a708d8e6ba0f005ab4ad885e25248032e2
michaelhopwood <michael.hopwood@knights.ucf.edu>
2021-12-13 16:56:59 -0500

    add low-cap in AIT model

--- pvops/timeseries/models/AIT.py
+++ pvops/timeseries/models/AIT.py
@@ -159,6 +159,14 @@ class AIT(Processer, Predictor):
                                         self.model_terms_highcap,
                                         prod_col_dict)
         prod_df.loc[high_cap_mask, prod_col_dict["baseline"]] = predicted
+
+        # Low-capacity systems
+        low_cap_mask = prod_df[prod_col_dict['dcsize']] <= 1000
+        predicted = self.predict_subset(prod_df.loc[low_cap_mask, :],
+                                        self.scaler_lowcap,
+                                        self.model_terms_lowcap,
+                                        prod_col_dict)
+        prod_df.loc[low_cap_mask, prod_col_dict["baseline"]] = predicted
         return prod_df
 
 
