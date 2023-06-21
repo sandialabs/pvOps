257d65768f0acd48ad2b31f5bfb44510b6bdc2f5
Charity Sotero <charity.sotero@kwhanalytics.com>
2023-06-21 13:32:37 +0000

    clean functions and make generic names

--- examples/text_classify_regex_example.py
+++ examples/text_classify_regex_example.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from nltk.tokenize import word_tokenize
 
-from pvops.text.classify_regex import add_equipment_labels
+from pvops.text.classify_regex import add_equipment_labels, EQUIPMENT_DICT
 
 LABEL_COLUMN = 'equipment_label'
 REGEX_LABEL_COLUMN = 'regex_equipment_label'
@@ -26,8 +26,15 @@ class Example:
         # tokenize notes to words
         self.df[NOTES_COLUMN] = self.df[NOTES_COLUMN].apply(word_tokenize)
 
+    def add_equipment_labels(self):
+        text_col = NOTES_COLUMN
+        new_col = REGEX_LABEL_COLUMN
+        reference_dict = EQUIPMENT_DICT
+        self.df = add_equipment_labels(self.df, text_col, new_col, reference_dict)
+
 if __name__ == "__main__":
-    # python -m pvops.text.classify_regex
+    # python -m examples.classify_regex_example
     df = get_sample_data()
     e = Example(df=df, REGEX_LABEL_COLUMN=REGEX_LABEL_COLUMN)
     e.add_equipment_labels()
+    import pdb; pdb.set_trace()
--- pvops/text/classify_regex.py
+++ pvops/text/classify_regex.py
@@ -1,13 +1,9 @@
 
-# TODO: compare input, output of classify module
-
 """
-1. get expected input (CCRE data)
-2. run through regex function
-3. get expected output
+Temporary placeholder to hold classify by search functions
 """
 
-# first pass of equipment dictionary is based on
+# Default equipment dictionary is based on
 # https://www.osti.gov/servlets/purl/1872704
 # TODO: only currently takes single words, not phrases: 'string inverter', 'met station', etc.
 EQUIPMENT_DICT = {'combiner': ['combiner', 'comb', 'cb'],
@@ -25,28 +21,37 @@ EQUIPMENT_DICT = {'combiner': ['combiner', 'comb', 'cb'],
                   'wiring': ['wiring', 'wire', 'wires']
                   }
 
-# TODO: allow .csv input also
+def get_keywords_of_interest(list_of_txt, reference_dict=None):
+    """Find keywords of interest in list of strings from reference dict.
 
-def get_all_keywords_of_interest(list_of_txt, reference_dict=None):
-    """
-    if keywords of interest are in the list of text, return the keyword category
-    for example, if 'inverter' are in the list of text, return ['inverter']
+    If keywords of interest given in a reference dict are in the list of
+    strings, return the keyword category, or categories. For example,
+    if the string 'inverter' is in the list of text, return ['inverter'].
 
     Parameters
     ----------
-    list_of_txt: list
-        list of strings
-    reference_dict: dict
-    
+    list_of_txt : list of str
+        Tokenized text, functionally a list of string values.
+    reference_dict : dict with {'keyword': [list of synonyms]} or None
+        Reference dictionary to search for keywords of interest,
+        in the expected format
+        {'keyword_a':
+            ['keyword_a', 'keyword_a_synonym_0', 'keyword_a_synonym_1', keyword_a_synonym_2', ...],
+         'keyword_b':
+            ['keyword_b', 'keyword_b_synonym_0', 'keyword_b_synonym_1', keyword_b_synonym_2', ...],
+         ...}
+        If None, use default reference dictionary.
+        Note: This function can currently only handle single words, no n-gram functionality.
+
     Returns
     -------
-    included_equipment: list
-        list of strings (included equipment named)
-
+    included_equipment: list of str
+        List of keywords from reference_dict found in list_of_txt, can be more than one value.
     """
     text_to_search = set(list_of_txt)
     
     if reference_dict is None:
+        # use default equipment lookup if no reference dict is given
         reference_dict = EQUIPMENT_DICT
 
     equipment_keywords = set(reference_dict.keys())
@@ -54,15 +59,42 @@ def get_all_keywords_of_interest(list_of_txt, reference_dict=None):
 
     return included_equipment
 
-def add_equipment_labels(self):
-    """
-    manually add labels to mor text logs
-    dataframe with additional 'equipment_label' column
-    dependent on entries in EQUIPMENT_DICT
-    """
-    self.df[REGEX_LABEL_COLUMN] = self.df[NOTES_COLUMN].apply(get_all_keywords_of_interest)
+def add_keyword_labels(df, text_col, new_col, reference_dict=None):
+    """Find keywords of interest in specified column of dataframe, return as new column value.
 
-    # each multi-category now in its own row
-    # some logs have multiple equipment issues
-    self.df = self.df.explode(REGEX_LABEL_COLUMN)
+    If keywords of interest given in a reference dict are in the specified column of the dataframe,
+    return the keyword category, or categories. For example, if the string 'inverter'
+    is in the list of text, return ['inverter'].
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe to search for keywords of interest, must include text_col.
+    text_col : str
+        Column of tokenized text values, to search for keywords.
+    new_col :str
+        Column name of new label column.
+    reference_dict : dict with {'keyword': [list of synonyms]} or None
+        Reference dictionary to search for keywords of interest,
+        in the expected format
+        {'keyword_a':
+            ['keyword_a', 'keyword_a_synonym_0', 'keyword_a_synonym_1', keyword_a_synonym_2', ...],
+         'keyword_b':
+            ['keyword_b', 'keyword_b_synonym_0', 'keyword_b_synonym_1', keyword_b_synonym_2', ...],
+         ...}
+        If None, use default reference dictionary.
+        Note: This function can currently only handle single words, no n-gram functionality.
+
+    Returns
+    -------
+    df: pd.DataFrame
+        Input df with new_col added, where each found keyword is its own row, may result in
+        duplicate rows if more than one keywords of interest was found in text_col.
+    """
+    df[new_col] = df[text_col].apply(get_keywords_of_interest, reference_dict)
+
+    # each multi-category now in its own row, some logs have multiple equipment issues
+    df = df.explode(new_col)
+
+    return df
 
